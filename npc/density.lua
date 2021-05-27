48153f0121a86f720aeed90d3557e7ea52f1af0b
Neofliix <chollet-nicolas@hotmail.fr>
2021-05-27 09:33:13 +0200

    NPC density management

---
+++ npc/density.lua
@@ -0,0 +1,12 @@
+-- NPC density management | Gestion de la densité des PNJ
+DensityMultiplier = 0.5
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
+	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+	end
+end)
